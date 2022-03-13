9599b7940de7b19692c2ade2e34c89572a61ffec
Nikita <nrffg@inbox.ru>
2022-03-13 20:40:32 +0700

    ПЗ5_ПР3

---
+++ PZ4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ5_PR3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Spinka first = new Spinka();
+            first.SetHeight(40);
+            first.SetLeg(4);
+            first.SetMaterial("Дерево");
+            first.SetHeight_Spinka(21);
+            first.PrintObject();
+
+        }
+    }
+}
---
+++ PZ4/Spinka.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ5_PR3
+{
+    public class Spinka:Taburet
+    {
+        protected int Height_Spinka;
+
+        public void SetHeight_Spinka(int Height_Spinka)
+        {
+            if (Height_Spinka >= Height * 0.5 && Height_Spinka <= Height)
+            {
+                this.Height_Spinka = Height_Spinka;
+                
+            }
+            else
+            {
+                Console.WriteLine("Кол-во не входит в диапазон");
+            }
+        }
+        
+        public override void PrintObject()
+        {
+            Console.WriteLine("Высота седушки: " + Height + "| Кол-во ножек: " + Leg + "| Материал изделия: " + Material + "| Высота спинка: " + Height_Spinka);
+        }
+    }
+}
