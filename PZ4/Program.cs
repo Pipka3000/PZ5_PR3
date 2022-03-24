f61e38d86b3bfbcc89be800d91a4ac94db7e1ec0
Гура Никита Станиславович гр. ИС(ПРО)-31 <is12756@stud.KHPK.RU>
2022-03-24 15:27:25 +0700

    enum

---
+++ PZ4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ5_PR3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taburet seccond = new Taburet();
+            Spinka first = new Spinka();
+            first.SetHeight(40);
+            first.SetLeg(4);
+            first.SetHeight_Spinka(21);
+            first.PrintObject();
+            
+            seccond._Material = Material.Дерево;
+        }
+    }
+}
---
+++ PZ4/Taburet.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ5_PR3
+{
+    public enum Material
+    {
+        Дерево = 0,
+        Пластик = 1,
+        Металл = 2
+    }
+
+    public class Taburet
+    {
+       
+
+        protected int Height;
+        protected int Leg;
+
+        Material Material;
+
+        public Taburet(int Height, int Leg, Material Material)
+        {
+            if (Height >= 20 && Height <= 60)
+            {
+                this.Height = Height;
+            }
+            else
+            {
+                Console.WriteLine("Кол-во не входит в диапазон");
+            }
+
+            if (Leg == 1 || Leg == 2 || Leg == 4)
+            {
+                this.Leg = Leg;
+            }
+            else
+            {
+                Console.WriteLine("Кол-во не входит в диапазон");
+            }
+
+            
+        }
+
+        //Высота сидушки
+        public void SetHeight(int Height)
+        {
+            if (Height >= 20 && Height <= 60)
+            {
+                this.Height = Height;
+            }
+            else
+            {
+                Console.WriteLine("Кол-во не входит в диапазон");
+            }
+        }
+        
+        //Количество ножек
+        public void SetLeg(int Leg)
+        {
+            if (Leg == 1 || Leg == 3 || Leg == 4)
+            {
+                this.Leg = Leg;
+            }
+            else
+            {
+                Console.WriteLine("Кол-во не входит в диапазон");
+            }
+        }
+        
+        //Вид материала изделия
+        public Material _Material
+        {
+            get { return Material; }
+            set
+            {
+                Material = value;
+            }
+        }
+
+
+        //Вывод всех данных
+        public virtual void PrintObject()
+        {
+            Console.WriteLine("Высота седушки: " + Height + "| Кол-во ножек: " + Leg + "| Материал изделия: " + Material);
+        }
+        
+        
+        
+        public Taburet()
+        {
+
+        }
+    }
+}
